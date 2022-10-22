7feda6a5b44f3ab6aaa67c66dfc4aac043621595
Allan Medeiros Minato <allan.minato@gmail.com>
2022-10-22 02:30:21 -0300

    Resolução exercicio 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (student) => {
+  const obj = {
+    name: student,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return obj;
+};
 
 module.exports = createStudent;
