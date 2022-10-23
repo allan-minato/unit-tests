4e876a40477a107680638ff07bd8d2fecc1184a9
Allan Medeiros Minato <allan.minato@gmail.com>
2022-10-23 20:45:16 -0300

    Resolução exercicio 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,15 +15,16 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
+// eslint-disable-next-line complexity
 const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
